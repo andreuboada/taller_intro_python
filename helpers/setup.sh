1b1b2d2a2f196f86c130c695e9fa8889f38eb471
Andreu Boada de Atela <andreuboadadeatela@Andreus-Mac.local>
2022-10-06 05:47:29 -0500

    Modificamos setup

--- helpers/setup.sh
+++ helpers/setup.sh
@@ -5,3 +5,7 @@ echo "Obteniendo librería auxiliar..."
 mkdir -p helpers
 curl -s -o helpers/helpers.py https://raw.githubusercontent.com/andreuboada/taller_intro_python/main/helpers/helpers.py
 echo "Proceso terminado 🍻..."
+
+echo "Obteniendo datos..."
+curl -s -o https://raw.githubusercontent.com/andreuboada/taller_intro_python/main/data/winequality-red.csv
+curl -s -o https://raw.githubusercontent.com/andreuboada/taller_intro_python/main/data/winequality-white.csv
