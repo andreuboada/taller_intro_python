6a3d8d1cdc5c4af0f7f37040563d3e94ef9becea
Andreu Boada de Atela <andreuboadadeatela@Andreus-Mac.local>
2022-11-17 01:24:52 -0600

    Agregamos rainy days

--- helpers/setup.sh
+++ helpers/setup.sh
@@ -11,5 +11,6 @@ mkdir -p data
 curl -s -o data/winequality-red.csv https://raw.githubusercontent.com/andreuboada/taller_intro_python/main/data/winequality-red.csv
 curl -s -o data/winequality-white.csv https://raw.githubusercontent.com/andreuboada/taller_intro_python/main/data/winequality-white.csv
 curl -s -o data/president_heights.csv https://raw.githubusercontent.com/andreuboada/taller_intro_python/main/data/president_heights.csv
+curl -s -o data/Seattle2014.csv https://raw.githubusercontent.com/andreuboada/taller_intro_python/main/data/Seattle2014.csv
 
 echo "Proceso terminado 🍻..."
